a936b2aff60f3c599bcc35d37e737cb3340d2588
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2023-03-12 15:13:11 +0600

    add banner footer

---
+++ src/Router/Router.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Component/Home/Home";
+import Main from "../Layout/Main";
+import SignIn from "../Shared/SignIn/SignIn";
+import SignUp from "../Shared/SignUp/SignUp";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/signUp",
+        element: <SignUp/>
+      },
+      {
+        path: "/signIn",
+        element: <SignIn/>
+      },
+    ],
+  },
+]);
+
+export default router;
